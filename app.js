5c8d4bdb2c5c1acc619dc4f92c36fe1b4324e360
David Martinez <34782922+dmtzt@users.noreply.github.com>
2022-01-23 19:12:39 -0600

    Request params and queries

--- app.js
+++ app.js
@@ -2,12 +2,53 @@ const express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
+const {products} = require('./data')
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, '/app', 'index.html'))
-// })
+app.get('/', (req, res) => {
+    res.send('<h1>Home Page</h1><a href="/api/products">Products</a>')
+})
+
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((product) => {
+        const {id, name, image} = product
+        return {id, name, image}
+    })
+
+    res.json(newProducts)
+})
+
+app.get('/api/products/:id', (req, res) => {
+    const singleProduct = products.find(
+        (product) => product.id == req.params.id)
+
+    if (!singleProduct)
+        return res.status(404).send('Product does not exist')
+
+    return res.json(singleProduct)
+})
+
+app.get('/api/v1/query', (req, res) => {
+    // console.log(req.query)
+    const {search, limit} = req.query
+    let sortedProducts = [...products]
+
+    if (search) {
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.includes(search)
+        })
+    }
+    if (limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    
+    if (sortedProducts.length < 1)
+        // res.status(200).send('no products matched your search')
+        return res.status(200).json({success: true, data: []})
+
+    res.status(200).json(sortedProducts)
+})
 
 app.all('*', (req, res) => {
     res.status(404).send('resource not found')
